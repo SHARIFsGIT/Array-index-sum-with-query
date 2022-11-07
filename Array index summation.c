b976fdb2d7779ef1b38acdb543a7e85f6ad45e4a
Shariful <sharifaiub15@gmail.com>
2022-11-07 06:26:47 +0100

    working code

---
+++ Array index summation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N, i, Q, j, index, value;
+    printf("Array size: ");
+    scanf("%d", &N);
+
+    int array[N];
+
+    printf("Array value: ");
+    for (i=0; i<N; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Queries: ");
+    scanf("%d", &Q);
+
+    for (j=0; j<Q; j++)
+    {
+        scanf("%d %d", &index, &value);
+        array[index]=array[index]+value;
+    }
+
+    for (i=0; i<N; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    return 0;
+}
+
